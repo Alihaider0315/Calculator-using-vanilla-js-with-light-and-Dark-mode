d1798607ea80a45082f86c711091bd82471902a3
ahti3301 <ahte02254@gmail.com>
2022-02-09 21:53:11 +0500

    adding trignometric functionality

--- app.js
+++ app.js
@@ -9,15 +9,37 @@ buttons.forEach((btn) => {
       let string = display.innerText.toString();
       display.innerText = string.slice(0, string.length - 1); //or substr(0,string.length-1)
     } else if (btn.id == "equal") {
-      let ans = eval(display.innerText);
-      if (ans == undefined) {
+      new_str = display.innerText.toString();
+      if (new_str == "") {
         display.innerText = "Error!";
         setTimeout(() => {
           display.innerText = " ";
         }, 1300);
+      } else if (new_str.includes("sin")) {
+        let text = new_str.slice(4, -1);
+        let ans = eval(Math.sin(Number(text)));
+        display.innerText = ans.toFixed(4);
+      } else if (new_str.includes("cos")) {
+        let text = new_str.slice(4, -1);
+        let ans = eval(Math.cos(Number(text)));
+        display.innerText = ans.toFixed(4);
+      } else if (new_str.includes("tan")) {
+        let text = new_str.slice(4, -1);
+        let ans = eval(Math.tan(Number(text)));
+        display.innerText = ans.toFixed(4);
       } else {
-        display.innerText = ans;
+        display.innerText = eval(display.innerText);
       }
+
+      // let ans = eval(display.innerText);
+      // if (ans == undefined) {
+      //   display.innerText = "Error!";
+      //   setTimeout(() => {
+      //     display.innerText = " ";
+      //   }, 1300);
+      // } else {
+      //   display.innerText = ans;
+      // }
     } else {
       display.innerText += btn.id;
     }
